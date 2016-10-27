72a8a02fe4ef0f90b90bcb98a991dd14152d7579
datchiz <datchiz5@gmail.com>
2016-10-27 20:41:45 +0700

    fix 2

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +12,7 @@ package test;
 public class Test {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments con heo
      */
     public static void main(String[] args) {
         Pig p1 = new Pig("Heo Lái");
