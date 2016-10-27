b08b51717a1a563d91d5ca4ccd574d54e610867e
datchiz <datchiz5@gmail.com>
2016-10-27 18:51:13 +0700

    frist

---
+++ src/test/Pig.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author MyPC
+ */
+public class Pig {
+    private String kind;
+
+    public Pig(String kind) {
+        this.kind = kind;
+    }
+
+    public Pig() {
+    }
+    
+    
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return kind;
+    }
+    
+    
+}
---
+++ src/test/Test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author MyPC
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pig p1 = new Pig("Heo Lái");
+        Pig p2 = null;
+        Pig p3 = new Pig("Heo Béo");
+        
+        p2 = p1;
+        p1 = p3;
+        
+        p1.setKind("Heo Bự");
+        
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+    }
+    
+}
